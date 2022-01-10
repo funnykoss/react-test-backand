eeb26e9013d6db716f57b3fb7c71fe975782dcd2
funnykoss <75586141+funnykoss@users.noreply.github.com>
2022-01-10 21:54:37 +0200

    current

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -19,4 +19,6 @@ router.post("/login", validation(joiSchema), controllerWrapper(ctrl.login));
 
 router.get("/logout", authenticate, controllerWrapper(ctrl.logout));
 
+router.get("/logout", authenticate, controllerWrapper(ctrl.current));
+
 module.exports = router;
